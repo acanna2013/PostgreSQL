22433f7991e333613dceca631ea833f8f75f442c
Anna Chen <51142303+acanna2013@users.noreply.github.com>
2022-05-24 20:42:48 -0400

    Update JoinExercises.sql

--- Joins/JoinExercises.sql
+++ Joins/JoinExercises.sql
@@ -3,3 +3,5 @@ SELECT cd.bookings.starttime
 FROM cd.bookings
 INNER JOIN cd.members ON cd.members.memid = cd.bookings.memid
 WHERE cd.members.surname = 'Farrell' AND cd.members.firstname = 'David';
+
+-- How can you produce a list of the start times for bookings for tennis courts, for the date '2012-09-21'? Return a list of start time and facility name pairings, ordered by the time.
