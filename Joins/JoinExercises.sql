8d27fa4b104c214edc657eb1ca70c2267832df1a
Anna Chen <51142303+acanna2013@users.noreply.github.com>
2022-05-24 20:43:02 -0400

    Update JoinExercises.sql

--- Joins/JoinExercises.sql
+++ Joins/JoinExercises.sql
@@ -6,3 +6,8 @@ WHERE cd.members.surname = 'Farrell' AND cd.members.firstname = 'David';
 
 -- How can you produce a list of the start times for bookings for tennis courts, for the date '2012-09-21'? 
 -- Return a list of start time and facility name pairings, ordered by the time.
+SELECT cd.bookings.starttime, cd.facilities.name
+FROM cd.bookings
+INNER JOIN cd.facilities ON cd.facilities.facid = cd.bookings.facid
+WHERE cd.facilities.name LIKE '%Tennis Court%' AND cd.bookings.starttime >= ('2012-09-21') AND cd.bookings.starttime < ('2012-09-22')
+ORDER BY cd.bookings.starttime;
