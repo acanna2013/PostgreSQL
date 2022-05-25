b0b55ac7958f913f05dd546ba5e731ffc1ddce84
Anna Chen <51142303+acanna2013@users.noreply.github.com>
2022-05-25 19:39:48 -0400

    Update JoinExercises.sql

--- Joins/JoinExercises.sql
+++ Joins/JoinExercises.sql
@@ -35,3 +35,19 @@ INNER JOIN cd.bookings ON cd.members.memid = cd.bookings.memid
 INNER JOIN cd.facilities ON cd.facilities.facid = cd.bookings.facid
 WHERE cd.facilities.name LIKE '%Tennis Court%'
 ORDER BY member, facility;
+
+-- How can you produce a list of bookings on the day of 2012-09-14 which will cost the member (or guest) more than $30? 
+-- Remember that guests have different costs to members (the listed costs are per half-hour 'slot'), 
+-- and the guest user is always ID 0. 
+-- Include in your output the name of the facility, the name of the member formatted as a single column, and the cost. 
+-- Order by descending cost, and do not use any subqueries.
+SELECT cd.members.firstname || ' ' || cd.members.surname AS member, cd.facilities.name AS facility, 
+CASE
+WHEN cd.members.memid = 0 THEN cd.bookings.slots*cd.facilities.guestcost
+ELSE cd.bookings.slots*cd.facilities.membercost
+END AS cost
+FROM cd.members
+INNER JOIN cd.bookings ON cd.members.memid = cd.bookings.memid
+INNER JOIN cd.facilities ON cd.facilities.facid = cd.bookings.facid
+WHERE cd.bookings.starttime >= '2012-09-14' AND cd.bookings.starttime < '2012-09-15' AND ((cd.members.memid = 0 AND guestcost*cd.bookings.slots > 30) OR (cd.members.memid != 0 AND membercost*cd.bookings.slots > 30))
+ORDER BY cost DESC;
