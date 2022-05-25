7c93f5a25b89e95f18daef6b97acda509a906fc2
Anna Chen <51142303+acanna2013@users.noreply.github.com>
2022-05-24 20:42:31 -0400

    Update JoinExercises.sql

--- Joins/JoinExercises.sql
+++ Joins/JoinExercises.sql
@@ -1,1 +1,5 @@
 -- How can you produce a list of the start times for bookings by members named 'David Farrell'?
+SELECT cd.bookings.starttime
+FROM cd.bookings
+INNER JOIN cd.members ON cd.members.memid = cd.bookings.memid
+WHERE cd.members.surname = 'Farrell' AND cd.members.firstname = 'David';
