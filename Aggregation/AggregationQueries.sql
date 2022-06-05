9f2bd92e44fdf30d8551e5afe6d4373fcb7ecc54
Anna Chen <51142303+acanna2013@users.noreply.github.com>
2022-06-04 21:36:14 -0400

    Update AggregationQueries.sql

--- Aggregation/AggregationQueries.sql
+++ Aggregation/AggregationQueries.sql
@@ -8,3 +8,17 @@ SELECT COUNT(*)
 FROM cd.facilities
 WHERE guestcost >= 10;
 
+-- Produce a count of the number of recommendations each member has made. Order by member ID.
+SELECT recommendedby, COUNT(*)
+FROM cd.members
+WHERE recommendedby is not null
+GROUP BY recommendedby
+ORDER BY recommendedby;
+
+-- Produce a list of the total number of slots booked per facility. 
+-- For now, just produce an output table consisting of facility id and slots, sorted by facility id.
+SELECT fc.facid, SUM(bks.slots)
+FROM cd.facilities fc
+INNER JOIN cd.bookings bks ON bks.facid = fc.facid
+GROUP BY fc.facid
+ORDER BY fc.facid;
