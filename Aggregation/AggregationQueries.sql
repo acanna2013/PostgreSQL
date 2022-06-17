9ca02c9dbca83234cc8eb07ae962f74d3911a374
Anna Chen <51142303+acanna2013@users.noreply.github.com>
2022-06-17 16:42:11 -0400

    Update AggregationQueries.sql

--- Aggregation/AggregationQueries.sql
+++ Aggregation/AggregationQueries.sql
@@ -133,6 +133,13 @@ SELECT facid, total from (
   ) as ranked
   where rank = 1;
   
--- 
-
+-- Produce a list of members (including guests), along with the number of hours they've booked in facilities, rounded to the nearest ten hours. 
+-- Rank them by this rounded figure, producing output of first name, surname, rounded hours, rank. Sort by rank, surname, and first name.
+SELECT firstname, surname, 
+((SUM(bks.slots)+10)/20)*10,
+RANK() OVER(ORDER BY ((SUM(bks.slots)+10)/20)*10 DESC) rank
+FROM cd.bookings bks
+INNER JOIN cd.members m ON bks.memid = m.memid
+GROUP BY m.memid
+ORDER BY rank, surname, firstname;
 
